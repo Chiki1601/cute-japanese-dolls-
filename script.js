8b4496baf2e4607d0d59e5a022a795e61df63520
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-09 15:28:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+/*
+Inspired from the illustration by unknown artist
+Link: https://previews.123rf.com/images/sabbracadabra/sabbracadabra1706/sabbracadabra170600007/79808112-japanese-kokeshi-dolls-vector-icons.jpg
+*/
